9f3e30f970b59c8a016c321513828b0d01f57038
Michael Astalos <michaeldont@gmail.com>
2021-07-21 13:07:33 -0400

    outlining implementation of Jquery to capturing user input and save to localStorage

--- assets/js/java.js
+++ assets/js/java.js
@@ -12,13 +12,15 @@ function generatePlanner() {
         sectionRow.className = 'row';
         var divTime = document.createElement('div');
         divTime.className = 'time-block';
+        //--handling time displayed for row 
         var h2Hour = document.createElement('h2');
         h2Hour.className = 'hour';
         if ((9+i) % 12>=9){h2Hour.innerText = ((9+i) % 12)+"AM";}
         else if ((9+i) % 12==0){h2Hour.innerText = "12PM";}
         else {h2Hour.innerText = ((9+i) % 12)+"PM"}
-        var divDescription = document.createElement('div');
 
+        //--handling row color for past/present/future
+        var divDescription = document.createElement('div');
         if(parseInt(moment(h2Hour.innerText,'ha').format('H')) == parseInt(currentHour))
         {divDescription.className = 'description present';}
         else if(parseInt(moment(h2Hour.innerText,'ha').format('H')) < parseInt(currentHour))
@@ -26,8 +28,11 @@ function generatePlanner() {
         else if(parseInt(moment(h2Hour.innerText,'ha').format('H')) > parseInt(currentHour))
         {divDescription.className = 'description future';}
 
+        //--handling textarea information + localStorage
         var textArea = document.createElement('textarea');
+        textArea.dataset.row = i;
         var btnSave = document.createElement('dutton');
+        btnSave.dataset.btn = i;
         btnSave.className = 'saveBtn';
         
         contentContainer.appendChild(sectionRow);
@@ -39,5 +44,14 @@ function generatePlanner() {
 
     }
 }
+
+$('.container').on('click', '.saveBtn' , function() {
+    var selectedBtn = $(this).attr('data-btn');
+    var userInput = $('textarea[data-row="'+selectedBtn+'"]');
+    // alert(typeof(selectedBtn))
+    console.log('data-row="'+selectedBtn+'"')
+    console.log(userInput)
+
+});
 
 generatePlanner();
