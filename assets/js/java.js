f6ae118c39af2ebc5d9706bd6086fa4680056b21
Michael Astalos <michaeldont@gmail.com>
2021-07-21 00:02:02 -0400

    stuck on row class assignment to present past or future classes

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,7 +1,12 @@
-var currentDateContainer = document.getElementById('currentDay');
+var currentDateContainer = document.querySelector('#currentDay');
 var contentContainer = document.querySelector('.container');
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentHour = moment('10 AM', 'hh A').format('hh');
+if(currentHour<10){currentHour = currentHour.replace("0")}
+var typography = moment('10 AM', 'hh A').format('A');
 
 //creations
+currentDateContainer.innerHTML = currentTime;
 
 function generatePlanner() {
     for(i=0;i<9;i++){
@@ -14,10 +19,32 @@ function generatePlanner() {
         if ((9+i) % 12>=9){h2Hour.innerText = ((9+i) % 12)+"AM";}
         else if ((9+i) % 12==0){h2Hour.innerText = "12PM";}
         else {h2Hour.innerText = ((9+i) % 12)+"PM"}
-        
-        if (h2Hour.innerText==0){h2Hour.innerText=12;}
         var divDescription = document.createElement('div');
-        divDescription.className = 'description';
+        debugger;
+        console.log(currentHour+typography+ " |||| "+h2Hour.innerText+" ||||"+currentHour +" |||"+((9+i) % 12))
+        console.log(i+" "+h2Hour.innerText.indexOf(currentHour+typography)+" |||")
+        
+        // switch (typography){
+        //     case "AM":
+        //         if (h2Hour.innerText.indexOf(currentHour+typography) == 0) 
+        //         {divDescription.className = 'description present';}
+        //         else if(currentHour >((9+i) % 12))
+        //         {divDescription.className = 'description past';}
+        //         else if(currentHour <((9+i) % 12))
+        //         {divDescription.className = 'description future';}
+        //         break;
+        //     case "PM":
+        //         if (h2Hour.innerText.indexOf(currentHour+typography) == 0) 
+        //         {divDescription.className = 'description present';}
+        //         else if(currentHour >((9+i) % 12) || currentHour ==12)
+        //         {divDescription.className = 'description past';}
+        //         else if(currentHour <((9+i) % 12))
+        //         {divDescription.className = 'description future';}
+        //         break;
+        // }
+
+
+        // divDescription.className = 'description';
         var textArea = document.createElement('textarea');
         var btnSave = document.createElement('dutton');
         btnSave.className = 'saveBtn';
