801dede6888e2e1d5039682f9ee7cff1d8c010ee
Michael Astalos <michaeldont@gmail.com>
2021-07-21 23:15:00 -0400

    finished up code on localStorage

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,18 +1,21 @@
 var currentDateContainer = document.querySelector('#currentDay');
 var contentContainer = document.querySelector('.container');
-var currentTime = moment().format('MMMM Do YYYY, H:mm:ss a');
+var currentTime = moment().format('dddd, MMMM Do');
 var currentHour = moment().format('H');
-var storage = [];
-
-//creations
+var storage = [{"saveBtnPress":"0","textAreaInput":""},{"saveBtnPress":"1","textAreaInput":""},{"saveBtnPress":"2","textAreaInput":""},
+{"saveBtnPress":"3","textAreaInput":""},{"saveBtnPress":"4","textAreaInput":""},{"saveBtnPress":"5","textAreaInput":""},
+{"saveBtnPress":"6","textAreaInput":""},{"saveBtnPress":"7","textAreaInput":""},{"saveBtnPress":"8","textAreaInput":""}];
 currentDateContainer.innerHTML = currentTime;
 
+//creating HTML elements
 function generatePlanner() {
+    var storage = JSON.parse(localStorage.getItem('plan'));
     for(i=0;i<9;i++){
         var sectionRow = document.createElement('section');
         sectionRow.className = 'row';
         var divTime = document.createElement('div');
         divTime.className = 'time-block';
+
         //--handling time displayed for row 
         var h2Hour = document.createElement('h2');
         h2Hour.className = 'hour';
@@ -22,16 +25,15 @@ function generatePlanner() {
 
         //--handling row color for past/present/future
         var divDescription = document.createElement('div');
-        if(parseInt(moment(h2Hour.innerText,'ha').format('H')) == parseInt(currentHour))
-        {divDescription.className = 'description present';}
-        else if(parseInt(moment(h2Hour.innerText,'ha').format('H')) < parseInt(currentHour))
-        {divDescription.className = 'description past';}
-        else if(parseInt(moment(h2Hour.innerText,'ha').format('H')) > parseInt(currentHour))
-        {divDescription.className = 'description future';}
+        if(parseInt(moment(h2Hour.innerText,'ha').format('H')) == parseInt(currentHour)) {divDescription.className = 'description present';}
+        else if(parseInt(moment(h2Hour.innerText,'ha').format('H')) < parseInt(currentHour)) {divDescription.className = 'description past';}
+        else if(parseInt(moment(h2Hour.innerText,'ha').format('H')) > parseInt(currentHour)) {divDescription.className = 'description future';}
 
-        //--handling textarea information + localStorage
+        //--handling textarea information + assigning saved data from localStorage
         var textArea = document.createElement('textarea');
         textArea.dataset.row = i;
+        if (storage[i].textAreaInput!=='') {textArea.innerText = storage[i].textAreaInput;}
+
         var btnSave = document.createElement('dutton');
         btnSave.dataset.btn = i;
         btnSave.className = 'saveBtn';
@@ -42,34 +44,22 @@ function generatePlanner() {
         sectionRow.appendChild(divDescription);
         divDescription.appendChild(textArea);
         sectionRow.appendChild(btnSave);
-
     }
 }
 
-function searchLS(btnPressed,array) {
-
-}
+//saveBtn click event
 $('.container').on('click', '.saveBtn' , function() {
     var saveBtnPress = $(this).attr('data-btn');
     var textAreaInput = $('[data-row="'+saveBtnPress+'"]').val();
 
-    planLS = JSON.parse(localStorage.getItem('plan'));
-    // localStorage.setItem("plan",JSON.stringify(storage));
-    planLS.forEach(function(item) {
-        // console.log(item.selectedBtn + ' || '+saveBtnPress)
-        // console.log(parseInt(item.selectedBtn) == parseInt(saveBtnPress))
-        if (parseInt(item.selectedBtn) == parseInt(saveBtnPress)){
-            console.log(item.userInput)
-            item.userInput = textAreaInput
-            localStorage.setItem('plan', JSON.stringify(planLS));
-            console.log(item.userInput)
+    //--updates textArea data in localStorage  
+    var storage = JSON.parse(localStorage.getItem('plan'));
+    storage.forEach(function(item) {
+        if (parseInt(item.saveBtnPress) == parseInt(saveBtnPress)){
+            item.textAreaInput = textAreaInput
+            localStorage.setItem('plan', JSON.stringify(storage));
         } 
     });
-    
-
-
-
-
 });
 
 generatePlanner();
